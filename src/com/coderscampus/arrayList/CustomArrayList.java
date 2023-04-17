78acee26c225fd286f367cbd8f4ab38ef823d9a2
Karen Dillehay <117693236+Kadillehay@users.noreply.github.com>
2023-04-17 10:27:19 -0500

    updated code

---
+++ src/com/coderscampus/arrayList/CustomArrayList.java
@@ -0,0 +1,91 @@
+package com.coderscampus.arrayList;
+
+import java.util.Arrays;
+
+public class CustomArrayList<T> implements CustomList<T> {
+
+	Object[] items = new Object[10];
+	int counter = 0;
+	int size = 0;
+
+	@Override
+	public boolean add(T item) {
+
+		items[counter] = item;
+		if (item == null) {
+			return false;
+		}
+		if (item != null) {
+			counter++;
+		}
+		if (counter == items.length && items != null) {
+			Object[] newItems = Arrays.copyOf(items, items.length * 2);
+			items = newItems;
+		}
+		return true;
+	}
+
+	@Override
+	public int getSize() {
+
+		int count = 0;
+
+		for (int i = 0; i < items.length; i++) {
+			if (items[i] != null) {
+				count++;
+//				size++;
+			}
+		}
+		return count++;
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public T get(int index) throws IndexOutOfBoundsException{
+		if (index < 0 || index > counter) {
+			throw new IndexOutOfBoundsException();
+		}
+		return (T) items[index];
+	}
+
+	@Override
+	public String toString() {
+		return "CustomArrayList [items=" + Arrays.toString(items);
+	}
+
+	@Override
+	public boolean add(int index, T item) throws IndexOutOfBoundsException {
+		if (index < 0 || index > counter) {
+			throw new IndexOutOfBoundsException();
+		}
+
+		if (counter == items.length) {
+			Object[] newItems = Arrays.copyOf(items, items.length * 2);
+			items = newItems;
+		}
+
+		// shift all elements to the right of the index by 1
+		for (int i = counter - 1; i >= index; i--) {
+			items[i + 1] = items[i];
+		}
+
+		items[index] = item;
+		counter++;
+		return true;
+	}
+
+	@Override
+	public T remove(int index) throws IndexOutOfBoundsException {
+	    if (index < 0 || index >= getSize()) {
+	        throw new IndexOutOfBoundsException();
+	    }
+	    T item = get(index);
+	    for (int i = index; i < getSize() - 1; i++) {
+	        items[i] = items[i + 1];
+	    }
+	    items[--counter] = null;
+	    return item;
+	}
+
+
+}
---
+++ test/com/coderscampus/arrayList/CustomApplicationTest.java
@@ -0,0 +1,157 @@
+package com.coderscampus.arrayList;
+
+import static org.junit.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class CustomApplicationTest {
+	Object[] items = new Object[10];
+	int counter = 0;
+
+	@Test
+	void should_add_item_to_list() {
+		CustomArrayList<String> sut = new CustomArrayList<>();
+		sut.add("testString");
+//		System.out.println(sut.get(0));
+		assertEquals("testString", sut.get(0));
+//		System.out.println("-----");
+	}
+
+	@Test
+	void should_add_30_items_to_list() {
+		CustomArrayList<Integer> sut = new CustomArrayList<>();
+		for (int i = 1; i <= 30; i++) {
+			sut.add(i);
+//			System.out.println(i);
+		}
+		assertEquals(30, sut.getSize());
+	}
+
+	@Test
+	void should_return_size_of_customArrayList() {
+		CustomArrayList<Integer> sut = new CustomArrayList<>();
+		for (int i = 1; i <= 38; i++) {
+			sut.add(i);
+		
+//			System.out.println(i);
+		}
+
+//		System.out.println("-------");
+//		System.out.println(sut.getSize());
+//		System.out.println("-------");
+		assertEquals (38, sut.getSize());
+	}
+	@Test
+	void should_add_item_at_specified_index() {
+		CustomArrayList<String> sut = new CustomArrayList<>();
+		sut.add(0, "blue");
+		sut.add(1, "red");
+		sut.add(2, "yellow");
+		sut.add(3, "green");
+		sut.add(4, "purple");
+		sut.add(5, "grey");
+		sut.add(6, "white");
+		sut.add(3, "magenta");
+		sut.add(7, "periwinkle");
+		sut.add(9, "lavender");
+		sut.add(9, "mauve");
+		sut.add(8, "red");
+		sut.add(11, "orange");
+		
+		assertEquals("magenta", sut.get(3));
+		assertEquals("orange", sut.get(11));
+		
+
+	}
+
+
+	@Test
+	void should_remove_item_from_customArrayList() {
+		var sut = new CustomArrayList<>();
+
+		sut.add(1);
+		sut.add(2);
+		sut.add(3);
+		sut.add(4);
+		sut.add(5);
+		sut.add(6);
+		sut.remove(2);
+//		System.out.println(sut);
+		assertEquals(5, sut.getSize());
+	}
+	
+	@Test
+	void should_remove_last_item_from_customArrayList() {
+		var sut = new CustomArrayList<>();
+//		for (int i = 1; i <= 11; i++) {
+//			sut.add(i);
+//		}
+		sut.add(9);
+		sut.add(9);
+		sut.add(9);
+		sut.add(9);
+		sut.add(9);
+		sut.add(9);
+		sut.remove(5);
+		System.out.println(sut);
+		assertEquals(5, sut.getSize());
+	}
+	
+	@Test
+	void should_return_size_of_array() {
+		CustomArrayList<String> sut = new CustomArrayList<>();
+		sut.add(0, "blue");
+		sut.add(1, "red");
+		sut.add(2, "yellow");
+		sut.add(3, "green");
+		sut.add(4, "purple");
+		sut.add(5, "grey");
+		sut.add(6, "white");
+		sut.add(3, "magenta");
+		sut.add(7, "periwinkle");
+		sut.add(9, "lavender");
+		sut.add(9, "mauve");
+		sut.add(8, "red");
+		sut.add(11, "orange");
+	
+		System.out.println(sut.getSize());
+		assertEquals(13, sut.getSize());
+		
+	}
+	@Test
+	void should_remove_item_from_beginning_of_list() {
+		CustomArrayList<String> sut = new CustomArrayList<>();
+		sut.add(0, "blue");
+		sut.add(1, "red");
+		sut.add(2, "yellow");
+		sut.add(3, "green");
+		sut.add(4, "purple");
+		sut.add(5, "grey");
+		sut.add(6, "white");
+		sut.add(3, "magenta");
+		sut.add(7, "periwinkle");
+		sut.add(9, "lavender");
+		sut.add(9, "mauve");
+		sut.add(8, "red");
+		sut.add(11, "orange");
+		sut.remove(0);
+//		System.out.println(sut.get(0));
+		assertEquals("red",sut.get(0));
+		
+	}
+	@Test 
+	void attempt_to_remove_null_item() {
+		CustomArrayList<Integer> sut = new CustomArrayList<>();
+		sut.add(1);
+		sut.add(2);
+		sut.add(3);
+		sut.add(4);
+		sut.add(5);
+		sut.add(6);
+		assertThrows(IndexOutOfBoundsException.class, () -> sut.remove(10));
+		System.out.println(sut);
+
+	
+	}
+}
